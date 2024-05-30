8f5c473a732f833af9b656c94764e36d0fc3fb59
HAPPY YADAV <happy.hy0017@gmail.com>
2024-05-30 13:44:51 +0530

    dataypes

---
+++ Practice/datatype.js
@@ -0,0 +1,17 @@
+"use strict"// new version of javascript use krne ke liye
+console.log(typeof null);
+//null type = object
+console.log(typeof undefined);
+//undefined type = undefined
+console.log(typeof {});
+//tyep =  object
+console.log(typeof []);
+//type =  object
+console.log(typeof '*')
+//type = string
+console.log(typeof 1)
+//type = number
+console.log(typeof true)
+//type = boolean
+console.log(typeof function(){})
+//type = function
