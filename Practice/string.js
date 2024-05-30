8b782f183818eb329becfba223cee7749db0ecc6
HAPPY YADAV <happy.hy0017@gmail.com>
2024-05-30 23:41:14 +0530

    strings, datatypes,  operations

---
+++ Practice/string.js
@@ -0,0 +1,19 @@
+// let number = "";
+// let num = Boolean(number);
+// console.log(num);//false
+
+// methods in strings
+let abc = new String("hap-p-y-yadav");
+// console.log(abc.length);
+console.table(abc);
+// console.log(abc.indexOf("y"));
+// console.log(abc.lastIndexOf("y"));
+// console.log(abc.search("y"));
+// console.log(abc.charAt(4))
+// console.log(abc.substring(0,3))
+// console.log(abc.slice(-7,3))
+// console.log(abc.trim());
+// console.log(abc.replace('h','H'))
+// console.log(abc.toUpperCase());
+// console.log(abc.split('-'));
+
