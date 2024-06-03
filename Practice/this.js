9e565fb903877a606f55e32cd0dfab0bef74cf22
HAPPY YADAV <happy.hy0017@gmail.com>
2024-06-03 12:46:36 +0530

    js

---
+++ Practice/this.js
@@ -0,0 +1,10 @@
+// function chai(){
+//     console.log(this);
+// }
+// chai(); // multiple objects
+
+//arrow function
+const chai = ()=>{
+    console.log(this);
+}
+chai(); // returns empty object
