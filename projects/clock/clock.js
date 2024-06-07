7f46a72ef5c6171a3ca6203a176eecdb99dd18df
HAPPY YADAV <happy.hy0017@gmail.com>
2024-06-07 13:01:12 +0530

    moved projects

--- projects/clock/clock.js
+++
@@ -1,7 +0,0 @@
-const clock=document.querySelector('#clock')
-// let date = new Date();
-// console.log(date.toLocaleTimeString())
-setInterval(() => {
-    let date = new Date()
-    clock.innerHTML=date.toLocaleTimeString()
-}, 1000);
